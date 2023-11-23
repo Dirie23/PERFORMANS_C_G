93b91d6cf4bdf5d38b71a212b7706b5936a0b4b1
Dirie23 <ahmedirie96@gmail.com>
2023-11-23 11:35:32 +0100

    product page version 1

---
+++ my-app/src/data/product_data.js
@@ -0,0 +1,41 @@
+const product_card = [
+
+{
+    id:1,
+    product_name: "Cykel",
+    description: "lorem lorem lorem lorem",
+    price: 299,
+    currency: "$",
+    thumb: "./images/cyckel.jpg"
+
+},
+
+{
+    id:2,
+    product_name: "Football",
+    description: "lorem lorem lorem lorem",
+    price: 99,
+    currency: "$",
+    thumb: "./images/football2.jpg" 
+},
+
+{
+    id:3,
+    product_name: "fotballskor",
+    description: "lorem lorem lorem lorem",
+    price: 199,
+    currency: "$",
+    thumb: "./images/fotballskor.jpg"
+},
+
+{
+    id:4,
+    product_name: "Sportcykel1",
+    description: "lorem lorem lorem lorem",
+    price: 19,
+    currency: "$",
+    thumb: "./images/sportcykel1.jpg"
+}
+
+]
+export default product_card;
